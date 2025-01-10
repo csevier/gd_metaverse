a19afbbc312bbab2e051f3722366f612bd46338c
cjs <sevierit@gmail.com>
2025-01-10 11:20:07 -0600

    async downloading

--- link.gd
+++ link.gd
@@ -4,25 +4,38 @@ class_name Link, "res://link.svg"
 export var link_location = "http://127.0.0.1:5000/"
 export var pck  = "test.pck"
 export var main_scene = "res://World.tscn"
+var thread
 
+func _ready():
+	print("adding to links")
+	add_to_group("links")
+	thread = Thread.new()
+	thread.start(self, "download")
+	
 func _http_request_completed(result, _response_code, _headers, _body):
 	if result != OK:
 		push_error("Download Failed")
-	var success = ProjectSettings.load_resource_pack("res://"+pck)
-	if success:
-		
-		var imported_scene = load(main_scene)
-		get_tree().change_scene_to(imported_scene)
-
-func download(link, path):
+	else:
+		print("World loaded from: " + link_location + pck)
+	
+func download():
 	var http = HTTPRequest.new()
 	add_child(http)
 	http.connect("request_completed", self, "_http_request_completed")
-	http.set_download_file(path)
-	var request = http.request(link)
+	http.set_download_file("user://"+pck)
+	var request = http.request(link_location+pck+"/")
 	if request != OK:
 		push_error("Http request error")
 
+func change_world():
+	var success = ProjectSettings.load_resource_pack("user://"+pck)
+	if success:
+		var imported_scene = load(main_scene)
+		get_tree().change_scene_to(imported_scene)
+
 func execute():
-	download(link_location+pck+"/", pck)
+	change_world()
+	
+func _exit_tree():
+	thread.wait_to_finish()
 
